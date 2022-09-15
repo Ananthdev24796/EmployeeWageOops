0280603deb8333f17cee27f559f1c5b46e6409e0
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-15 14:38:40 +0530

    Added: Usecase 5 Here we are iterate a day till 20 using control statement for loop

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -9,13 +9,14 @@ package com.bridgelabz;
  */
 public class EmployeeWage {
 	/**
-	 * UC4
-	 * using switch case statement
+	 * UC5
+	 * Calculating Wages for a Month assuming 20 Working Days in a Month
 	 */
 	//Constant
 		public static final  int IS_PART_TIME = 1;
 		public static final int IS_FULL_TIME = 2;
 		public static final int EMP_RATE_PER_HOUR = 20;
+		public static final int NUM_OF_WORKING_DAYS= 20;
 
 	
 	public static void main(String[] args) {
@@ -23,12 +24,14 @@ public class EmployeeWage {
 		//variables
 		int empHrs = 0;
 		int empWage = 0;
+		int totalEmpWage = 0;
 		//computation
+		for(int day =1;day<=NUM_OF_WORKING_DAYS;day++) {
 		int empCheck =(int) Math.floor(Math.random()*10)%3;
 		switch(empCheck){
 		
 		case IS_FULL_TIME:
-			System.out.println("Employee is Present for Part time");
+			System.out.println("Employee is Present for Part time	");
 			empHrs = 4;
 			break;
 		case IS_PART_TIME:
@@ -41,8 +44,10 @@ public class EmployeeWage {
 			break;
 		}
 		empWage = empHrs*EMP_RATE_PER_HOUR;
+		totalEmpWage += empWage;
 		System.out.println(empWage);
 
 	}
-
+		System.out.println(totalEmpWage);
+  }
 }

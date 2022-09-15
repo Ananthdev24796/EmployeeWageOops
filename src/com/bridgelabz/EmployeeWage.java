e2d0d0835c0138e31568ca0e98b21e1de470feaa
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-15 14:01:28 +0530

    Added: usecase 1 Here we are checking whether employee is present or absent

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -10,10 +10,19 @@ package com.bridgelabz;
 public class EmployeeWage {
 
 	/**
-	 * @param args
+	 * UC1
+	 * Check Employee is Present or Absent
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//Constant
+		int IS_FULL_TIME = 1;
+		//computation
+		double empCheck = Math.floor(Math.random()*10)%2;
+		if(empCheck == 1) {
+			System.out.println("Employee is Present");
+		}else {
+			System.out.println("Employee is Absent");
+		}
 
 	}
 

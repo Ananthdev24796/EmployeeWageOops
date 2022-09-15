3c30dce08077d5041db6df1ab7394baae91fbc84
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-15 14:12:02 +0530

    Added: useCase 3 Here we added new part time employee and calculate hime daily wages

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -10,21 +10,25 @@ package com.bridgelabz;
 public class EmployeeWage {
 
 	/**
-	 * UC2
-	 * Calculate Daily Employee Wage
+	 * UC3
+	 * Add Part time Employee & Wage
 	 */
 	public static void main(String[] args) {
 		//Constant
 		int IS_FULL_TIME = 1;
+		int IS_PART_TIME = 2;
 		int EMP_RATE_PER_HOUR = 20;
 		//variables
 		int empHrs = 0;
 		int empWage = 0;
 		//computation
-		double empCheck = Math.floor(Math.random()*10)%2;
+		double empCheck = Math.floor(Math.random()*10)%3;
 		if(empCheck == IS_FULL_TIME) {
-			System.out.println("Employee is Present");
+			System.out.println("Employee is Present for full time");
 			empHrs = 8;
+		}else if(empCheck == IS_PART_TIME) {
+			System.out.println("Employee is Present for Part time");
+			empHrs = 4;
 		}else {
 			System.out.println("Employee is Absent");
 			empHrs = 0;

8847fbc9277db0182db3f412bb705ef209e09992
Ananthdev24796 <ananth.cr7@gmail.com>
2022-09-15 15:23:28 +0530

    Removed constants

--- src/com/bridgelabz/EmployeeWage.java
+++ src/com/bridgelabz/EmployeeWage.java
@@ -15,9 +15,7 @@ public class EmployeeWage {
 	//Constant
 		public static final  int IS_PART_TIME = 1;
 		public static final int IS_FULL_TIME = 2;
-		public static final int EMP_RATE_PER_HOUR = 20;
-		public static final int NUM_OF_WORKING_DAYS= 20;
-		public static final int MAX_HR_IN_MONTH = 100; 
+	
 		
 
 	
